f3df42dec04020823c9a325be4ee1ba65b90063e
lionel <quyenltv54cntt@gmail.com>
2020-08-16 19:27:32 +0700

    update h1

--- wp-content/themes/main/template-parts/components/logo.php
+++ wp-content/themes/main/template-parts/components/logo.php
@@ -6,6 +6,10 @@ $logo = get_field('header_logo', 'option');
         if(is_front_page()){
     ?>
             <h1>
+                <span><?php
+	                echo get_bloginfo('name');
+	                ?>
+                </span>
                 <a href="<?=home_url()?>"><img src="<?=$logo['url']?>" alt="Gái Pro - Nguồn cảm hứng vô tận"></a>
             </h1>
     <?php
