57763a1457ea9192289ba7eaceccf3db542b3c86
lionel <quyenltv54cntt@gmail.com>
2020-08-22 18:15:52 +0700

    update taxonomy

--- wp-content/themes/main/taxonomy-khu_vuc.php
+++ wp-content/themes/main/taxonomy-khu_vuc.php
@@ -5,7 +5,7 @@ $queried_object = get_queried_object();
 	<!--Start Pull HTML here-->
     <div class="tax_title">
         <div class="container">
-            <h1 class="title"><?=__('Gái gọi ', DOMAIN)?><span><?=$queried_object->name?></h1>
+            <h1 class="title"><span><?=__('Gái gọi ', DOMAIN)?><?=$queried_object->name?></h1>
         </div>
     </div>
     <?php get_template_part('template-parts/blocks/tax/lists'); ?>
